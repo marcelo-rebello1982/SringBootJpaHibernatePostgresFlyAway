f160608ee5c2a2c5b7e910a69c9a760567e6d75f
marcelo-rebello1982 <mp.rebello.martins@gmail.com>
2021-09-15 16:32:36 -0300

    feature: inclusÃão swagger

--- src/main/java/com/postgres/config/SwaggerConfig.java
+++ src/main/java/com/postgres/config/SwaggerConfig.java
@@ -2,42 +2,39 @@ package com.postgres.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Header;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableSwagger2
-@Import(BeanValidatorPluginsConfiguration.class)
-public class SwaggerConfig {                                    
-    
+public class SwaggerConfig {
+
     @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build().apiInfo(getApiInfo());                                           
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+                .select().apis(RequestHandlerSelectors.basePackage("com.api.desafio.livros.controller"))
+                .paths(PathSelectors.any()).build().apiInfo(apiInfo());
     }
 
-    private ApiInfo getApiInfo() {
-        return new ApiInfo(
-                "Contact Application API",
-                "This is a sample Spring Boot RESTful service using SpringFox + Swagger 2",
-                "V1",
-                "urn:tos",
-                new Contact("Dariawan", "https://www.dariawan.com", "hello@dariawan.com"),
-                "CC BY-SA 3.0",
-                "https://creativecommons.org/licenses/by-sa/3.0/",
-                Collections.emptyList()
-        );
+    private ApiInfo apiInfo() {
+        return new ApiInfo("API xxx", "API REST  xxx", "Versão 1.0", "http://www.xxx.com.br",
+                new Contact("MPRM", "http://www.xxx.com.br", "mp.rebello.martins@gmail.com"), "opensource",
+                "http://www.xxx.com.br/terms", Collections.emptyList());
     }
+
 }
